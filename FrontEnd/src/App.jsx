d8b39903ddb32f778cbca79f22aa1ed45c6814b9
surendrayadavcse <venkatasurendra70@gmail.com>
2024-11-29 01:35:09 +0530

    first commit

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,52 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Register from './components/Register'
+import Login from './components/Login'
+import Notfound from './components/Notfound'
+import Track from './components/Track'
+
+import Private from './components/Private'
+
+import { UserContext } from './contexts/UserContext'
+import { useEffect, useState } from 'react'
+import Diet from './components/Diet'
+
+
+
+function App() {
+ 
+  const [loggedUser,setLoggedUser] = useState(JSON.parse(localStorage.getItem("nutrify-user")));
+
+  
+
+  return (
+    <>
+
+    
+      <UserContext.Provider value={{loggedUser,setLoggedUser}}>
+
+          <BrowserRouter>   
+
+              <Routes>
+
+                  <Route path='/' element={<Login/>}/>
+                  <Route path='/login' element={<Login/>}/>
+                  <Route path='/register' element={<Register/>}/>
+                  <Route path='/track' element={<Private Component={Track}/>}/>
+                  <Route path='/diet' element={<Private Component={Diet}/>}/>
+                  
+                  <Route path='*' element={<Notfound/>}/>
+
+              </Routes>
+        
+            </BrowserRouter>
+       
+
+        </UserContext.Provider>
+
+        
+    </>
+  )
+}
+
+export default App
